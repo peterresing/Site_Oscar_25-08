f37aecadf4b693e6e6d962c5ed3ea6297defd27c
Aluno SENAI <Aluno@SN-411399.edu.fiesc.com.br>
2026-09-03 21:39:27 -0300

    ta funcionando

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function limparTela() {
     const container = document.getElementById('conteudo-principal');
     container.innerHTML = ''; 
 }
+
+function pagina1() {
+    const abrir1 = document.getElementById('abrirpagina1');
+    abrir1.innerHTML = '';
+}
