d2876254defc056053965fbfc88b0c2fa7829aff
Aluno SENAI <Aluno@SN-351118.edu.fiesc.com.br>
2026-09-08 21:41:01 -0300

    salvado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function limparTela() {
-    const container = document.getElementById('conteudo-principal');
-    container.innerHTML = ''; 
+function abrirPaginas() {
+    document.getElementById('tela1').style.display = 'none';
+    document.getElementById('tela2').style.display = 'block';
 }
 
-function pagina1() {
-    const abrir1 = document.getElementById('abrirpagina1');
-    abrir1.innerHTML = '';
+function fecharPaginas() {
+    document.getElementById('tela1').style.display = 'flex';
+    document.getElementById('tela2').style.display = 'none';
 }
